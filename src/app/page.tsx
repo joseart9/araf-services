b29a2f9403598d7a40237d9176f3114716c314ea
Arturo <a01177145@tec.mx>
2025-03-21 19:13:41 -0600

    Remove overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Page() {
   return (
     <>
       <Nav />
-      <main>
+      <main className="overflow-x-hidden">
         <section id="home">
           <Hero />
         </section>
